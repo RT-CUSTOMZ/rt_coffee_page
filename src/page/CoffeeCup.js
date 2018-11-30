092d253692532002bf3736412e7371afd70c885a
Pia Piekarek <piapiekarek@googlemail.com>
2018-11-30 12:02:34 +0100

    change database queries

--- src/page/CoffeeCup.js
+++ src/page/CoffeeCup.js
@@ -3,20 +3,32 @@ import '../css/coffee_font.css';
 import './CoffeeCup.css';
 import {Paper} from '@material-ui/core'
 
+import 'moment-timezone';
+import 'moment/locale/de';
+
 export default class CoffeeCup extends React.Component {
     render() {
         const {current} = this.props;
         let currentCoffee = Object.keys(current).reduce(function (total, currentValue) {
             let entry = {
-                state : current[currentValue].state,
-                fill_level : current[currentValue].fill_level,
+                state : current[currentValue].coffee_machine.state,
+                time_state : current[currentValue].coffee_machine.time,
+                fill_level : current[currentValue].scale.fill_level,
+                time_fill_level: current[currentValue].scale.time,
             }
 
             return entry;
         },0);
         let Icon = "no_status";
         let CoffeeColor = "no";
-        if (currentCoffee.state === "coffee_fill_level") {
+        var moment = require('moment');
+        let timestamp_fillevel = moment(currentCoffee.time_fill_level).format("x");
+        let timestamp_coffeemachine = moment(currentCoffee.time_state).format("x");
+        console.log(timestamp_fillevel);
+        console.log(timestamp_coffeemachine);
+
+
+        if (timestamp_coffeemachine > timestamp_fillevel) {
             if(currentCoffee.fill_level >= 90){
                 Icon = "icon-coffee_cup_100";
                 CoffeeColor = "coffeeBackground_FillLevel_100";
@@ -31,13 +43,16 @@ export default class CoffeeCup extends React.Component {
                 CoffeeColor = "coffeeBackground_FillLevel_0";
             }
         }
-        if (currentCoffee.state === "coffee_brewing") {
-            Icon = "icon-coffee_machine_0";
-            CoffeeColor = "coffeeBackground_coffeeBrewing"
-        }
-        if (currentCoffee.state === "coffee_ready") {
-            Icon = "icon-coffee_machine_100";
-            CoffeeColor = "coffeeBackground_coffeeReady"
+        else
+        {
+            if (currentCoffee.state === "coffee_brewing") {
+                Icon = "icon-coffee_machine_0";
+                CoffeeColor = "coffeeBackground_coffeeBrewing"
+            }
+            if (currentCoffee.state === "coffee_ready") {
+                Icon = "icon-coffee_machine_100";
+                CoffeeColor = "coffeeBackground_coffeeReady"
+            }
         }
         return (
             <Paper className={CoffeeColor}>
--- src/page/Main.js
+++ src/page/Main.js
@@ -12,9 +12,10 @@ export default class Main extends React.Component {
 
         let currentCoffee = Object.keys(current).reduce(function (total, currentValue) {
             let entry = {
-                state : current[currentValue].state,
-                fill_level : current[currentValue].fill_level,
-                time: current[currentValue].time,
+                state : current[currentValue].coffee_machine.state,
+                time_state : current[currentValue].coffee_machine.time,
+                fill_level : current[currentValue].scale.fill_level,
+                time_fill_level: current[currentValue].scale.time,
             }
 
             return entry;
@@ -23,11 +24,15 @@ export default class Main extends React.Component {
         return (
             <Grid container justify="center" spacing={16}>
                 <Grid item>
+
                         <CoffeeCup current={current}/>
-                        F&uuml;llstand: {currentCoffee.fill_level} %<br/>
+                        Füllstand Kaffeekanne:{currentCoffee.fill_level} % <br/>
                         Status: {currentCoffee.state} <br/>
                         Zuletzt gekocht um: <Moment locale="de" tz="Europe/Paris" format="LTS">{currentCoffee.time}</Moment> <br/>
                         Das war: <Moment locale="de" tz="Europe/Paris" fromNow>{currentCoffee.time}</Moment> <br/>
+                </Grid>
+                <Grid item>
+
                 </Grid>
             </Grid>
         );
